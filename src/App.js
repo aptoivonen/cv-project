675a6b08a853747ce5c7d8177f9f7f165e505fe3
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-04 11:44:59 +0300

    Refactor state location

--- src/App.js
+++ src/App.js
@@ -1,67 +1,12 @@
-import { useState } from "react";
-import uniqid from "uniqid";
-
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
-  const [personalInfo, setPersonalInfo] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phoneNumber: "",
-  });
-  const [education, setEducation] = useState([]);
-  const [isSubmitted, setSubmitted] = useState(false);
-
-  const handlePersonalInfoChange = (data) => {
-    console.log("handlePersonalInfoChange:", data);
-    setPersonalInfo(data);
-  };
-
-  const handleEducationChange = (id, name, value) => {
-    const newEducation = education.map((edu) =>
-      edu.id === id
-        ? {
-            ...edu,
-            [name]: value,
-          }
-        : edu
-    );
-    setEducation(newEducation);
-  };
-
-  const handleEducationAdd = () => {
-    const newEducation = [
-      ...education,
-      { id: uniqid(), schoolName: "", titleOfStudy: "", dateOfStudy: "" },
-    ];
-    setEducation(newEducation);
-  };
-
-  const handleEducationDelete = (id) => {
-    const newEducation = education.filter((edu) => edu.id !== id);
-    setEducation(newEducation);
-  };
-
-  const handleSubmit = () => {
-    setSubmitted(true);
-  };
-
   return (
     <div>
       <Header />
-      <Main
-        personalInfo={personalInfo}
-        onPersonalInfoChange={handlePersonalInfoChange}
-        education={education}
-        onEducationChange={handleEducationChange}
-        onEducationAdd={handleEducationAdd}
-        onEducationDelete={handleEducationDelete}
-        isSubmitted={isSubmitted}
-        onSubmit={handleSubmit}
-      />
+      <Main />
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,106 @@
+import { useState } from "react";
+import uniqid from "uniqid";
 import Form from "./Form";
 import Result from "./Result";
 import "./Main.css";
 
-export default function Main({
-  personalInfo,
-  onPersonalInfoChange,
-  education,
-  onEducationChange,
-  onEducationAdd,
-  onEducationDelete,
-  isSubmitted,
-  onSubmit,
-}) {
+export default function Main() {
+  const [personalInfo, setPersonalInfo] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+  });
+  const [education, setEducation] = useState([]);
+  const [experience, setExperience] = useState([]);
+  const [isSubmitted, setSubmitted] = useState(false);
+
+  const handlePersonalInfoChange = (data) => {
+    setPersonalInfo(data);
+  };
+
+  const handleEducationChange = (id, name, value) => {
+    const newEducation = education.map((edu) =>
+      edu.id === id
+        ? {
+            ...edu,
+            [name]: value,
+          }
+        : edu
+    );
+    setEducation(newEducation);
+  };
+
+  const handleEducationAdd = () => {
+    const newEducation = [
+      ...education,
+      { id: uniqid(), schoolName: "", titleOfStudy: "", dateOfStudy: "" },
+    ];
+    setEducation(newEducation);
+  };
+
+  const handleEducationDelete = (id) => {
+    const newEducation = education.filter((edu) => edu.id !== id);
+    setEducation(newEducation);
+  };
+
+  const handleExperienceChange = (id, name, value) => {
+    const newExperience = experience.map((exp) =>
+      exp.id === id
+        ? {
+            ...exp,
+            [name]: value,
+          }
+        : exp
+    );
+    setExperience(newExperience);
+  };
+
+  const handleExperienceAdd = () => {
+    const newExperience = [
+      ...experience,
+      {
+        id: uniqid(),
+        companyName: "",
+        positionTitle: "",
+        mainTasks: "",
+        dateFrom: "",
+        dateUntil: "",
+      },
+    ];
+    setExperience(newExperience);
+  };
+
+  const handleExperienceDelete = (id) => {
+    const newExperience = experience.filter((exp) => exp.id !== id);
+    setExperience(newExperience);
+  };
+
+  const handleSubmit = () => {
+    setSubmitted(true);
+  };
+
   return (
     <main className="main">
       <div className="container">
         {isSubmitted ? (
-          <Result personalInfo={personalInfo} education={education} />
+          <Result
+            personalInfo={personalInfo}
+            education={education}
+            experience={experience}
+          />
         ) : (
           <Form
             personalInfo={personalInfo}
-            onPersonalInfoChange={onPersonalInfoChange}
+            onPersonalInfoChange={handlePersonalInfoChange}
             education={education}
-            onEducationChange={onEducationChange}
-            onEducationAdd={onEducationAdd}
-            onEducationDelete={onEducationDelete}
-            onSubmit={onSubmit}
+            onEducationChange={handleEducationChange}
+            onEducationAdd={handleEducationAdd}
+            onEducationDelete={handleEducationDelete}
+            onExperienceChange={handleExperienceChange}
+            onExperienceAdd={handleExperienceAdd}
+            onExperienceDelete={handleExperienceDelete}
+            onSubmit={handleSubmit}
           />
         )}
       </div>
